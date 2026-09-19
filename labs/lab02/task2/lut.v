eaf170f4ad5975191871ee520e7816d7de76ddf7
f20240742-oss <f20240742@pilani.bits-pilani.ac.in>
2026-09-19 10:36:22 +0000

    lab02 task2

--- labs/lab02/task2/lut.v
+++ labs/lab02/task2/lut.v
@@ -1,8 +1,6 @@
 // lut.v
 // A small parameterized ROM (lookup table): DEPTH words, each WIDTH bits
 // wide. dout continuously reflects mem[sel].
-//
-// YOU complete the two TODOs below. Everything else is given.
 
 module lut #(
   parameter WIDTH = 8,
@@ -14,15 +12,18 @@ module lut #(
 
   reg [WIDTH-1:0] mem [0:DEPTH-1];
 
+
   integer i;
 
-  // TODO: initialize mem[i] = i*i for every i from 0 to DEPTH-1.
-  // Use an initial block with a for loop -- this is the only place a ROM's
-  // contents should be set up. (See the lab manual for why.)
-  
+  // Initialize the ROM contents: mem[i] = i*i for i = 0 .. DEPTH-1.
+  initial begin
+    for (i = 0; i < DEPTH; i = i + 1)
+      mem[i] = i * i;
+  end
 
-  // TODO: make dout continuously reflect mem[sel]. This is a combinational
-  // read -- pick the right procedural block and sensitivity list.
-
+  // Combinational read: dout continuously reflects mem[sel].
+  always @(*) begin
+    dout = mem[sel];
+  end
 
 endmodule
--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,11 +1,24 @@
 // tb.v
-// Starter testbench template -- YOU complete this file.
+// Testbench for the lookup table (ROM) lut.v
 
 module tb;
 
-  // TODO: declare the inputs and outputs
+  // Must match the parameters used for the DUT below.
+  localparam WIDTH = 8;
+  localparam DEPTH = 4;
 
-  // TODO: instantiate DUT here
+  // Input is driven from procedural code -> reg; output comes from DUT -> wire.
+  reg  [$clog2(DEPTH)-1:0] t_sel;
+  wire [WIDTH-1:0]         t_dout;
+
+  // The instance is named DUT so that $dumpvars(0, DUT) below resolves to it.
+  lut #(
+    .WIDTH (WIDTH),
+    .DEPTH (DEPTH)
+  ) DUT (
+    .sel  (t_sel),
+    .dout (t_dout)
+  );
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
@@ -17,11 +30,23 @@ module tb;
   end
 
   initial begin
-    // TODO: apply different input combinations
+    // Read every location in order: expect 0, 1, 4, 9
+    t_sel = 0;
+    #5 t_sel = 1;
+    #5 t_sel = 2;
+    #5 t_sel = 3;
 
+    // Then jump around out of order to show the read is combinational
+    #5 t_sel = 2;
+    #5 t_sel = 0;
+    #5 t_sel = 3;
+    #5 t_sel = 1;
+
+
+    #5 $finish;
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " sel=%d | dout=%d", t_sel, t_dout);
 
 endmodule
